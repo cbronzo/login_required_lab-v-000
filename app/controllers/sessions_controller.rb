0a4dd9738eb6a2093abe94f38bf2e45ffe19237f
Catherine Bronzo <cbronzo@mac.com>
2018-10-04 21:47:14 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,12 @@
 class SessionsController < ApplicationController
   
   def create
+    if params[:name].nil? || params[:name].empty?
+      redirect_to '/login'
+    else
+      session[:name] = params[:name]
+      render '/secrets/show'
+    end
   end
 
   def destroy
