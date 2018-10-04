7e20d6b7ead7cc7135947b52d388871fd9eeeb48
Catherine Bronzo <cbronzo@mac.com>
2018-10-04 21:43:54 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,9 @@ class ApplicationController < ActionController::Base
     session[:name]
   end
   
+  private
+   def require_login
+    redirect_to controller: 'sessions', action: 'new' unless current_user
+  end
+  
 end
